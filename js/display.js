f32d2e7c85290c33757003c37cb895eec8871a01
rizaiya <jin20030127@icloud.com>
2023-09-24 23:28:48 +0900

    display画面途中

--- js/display.js
+++ js/display.js
@@ -14,4 +14,29 @@ function showDeleteConfirmation() {
       }
     });
   }
+
+  // JavaScriptコード（display.js）で、受け取った日付をHTMLに表示する
+// このコードは日付を表示する部分です。
+
+document.addEventListener("DOMContentLoaded", function() {
+  let numbers = document.getElementById("numbers");
+   // PHPから渡された日付を表示
+  let date_id = numbers.innerHTML; // 日付を取得
+
+
+
+axios.post("http://mute-iki-2515.moo.jp/sharing/display.php", {date_id: date_id})
+    .then(response => {
+      console.log(response.data);
+      if (response.data.success == 0) {
+        // window.location.href = "display.html";
+      }else if(response.data.success == 1) {
+        document.getElementById("errorMessage").innerText = "";
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+  });
   
